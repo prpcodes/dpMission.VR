e154703ff455d7ec736dbe2b16f41bc474d25d13
prp.wien <mail@prp.wien>
2020-12-30 14:15:15 +0100

    ADD: inventory class

--- Functions.hpp
+++ Functions.hpp
@@ -2,7 +2,14 @@ class Life_Client_Core {
     tag = "life";
 
     class DpMission {
-    	file = "core";
+    	file = "main\functions\dpMission";
 	    class getDpMission {};
+    };
+
+    class Inventory {
+        file = "main\functions\dialogs\inventory";
+        class inventory_get {};
+        class inventory_onClick {};
+        class inventory_addToList {};
     };
 };
